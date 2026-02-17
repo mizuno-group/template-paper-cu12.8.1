3be895556269bb395cf6252d1bbb56f77f817dd7
Honzawa-Ryu <benzelongji@gmail.com>
2026-02-17 16:55:03 +0900

    Update make_sif.sh

--- make_sif.sh
+++ make_sif.sh
@@ -2,9 +2,9 @@
 #SBATCH --partition=interactive
 #SBATCH --time=01:00:00
 
-rm -f testenv.sif
+rm -f env.sif
 
-apptainer build --fakeroot testenv.sif testenv.def
+apptainer build --fakeroot env.sif env.def
 
 if [ $? -eq 0 ]; then
     echo "Build successful!"
