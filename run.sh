978159c4e3c8b0a944d65782ab6563105380556c
Honzawa-Ryu <benzelongji@gmail.com>
2026-02-27 18:14:29 +0900

    Enhance run.sh with logging and environment setup
    Added environment variable declaration, output settings, and logging for execution results.

--- run.sh
+++ run.sh
@@ -5,25 +5,34 @@
 #SBATCH --gres=gpu:1
 #SBATCH --mem=16G
 
+# 環境変数として.bashrcを宣言
 source ~/.bashrc
-
 CONTAINER_IMAGE="env.sif"
+
+# sifがあるかどうかを確認、なければエラーを出す
 if [ ! -f $CONTAINER_IMAGE ]; then
-    echo ".sif file is not exist."
+    echo ".sif file does not exist."
     exit 1
 fi
 
+# ログなどの出力設定
 TIMESTAMP=$(date +%y%m%d_%H%M)
 OUT_DIR="results/${TIMESTAMP}_${SLURM_JOB_ID}"
+
+# 結果出力用のディレクトリを作成
 mkdir -p "$OUT_DIR"
 
+# 標準出力、エラー出力のファイル
 exec > >(tee -a "${OUT_DIR}/stdout.log")
 exec 2> >(tee -a "${OUT_DIR}/stderr.log" >&2)
 
+# 実行スクリプト、実行した際のgitコミット、そこからの差分を記録
 cp "$0" "$OUT_DIR/job_script.sh"
 git rev-parse HEAD > "$OUT_DIR/git_commit.txt"
 git diff > "$OUT_DIR/git_diff.patch"
 
+# 最新の結果を表示するlatest下のリンク作成
 ln -snf "$(realpath $OUT_DIR)" results/latest
 
+# コード実行
 apptainer exec --nv $CONTAINER_IMAGE bash -c "uv run python scripts/00_test.py"
