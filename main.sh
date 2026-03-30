f8a43882d4db93803439c7e6963c0066571af2d2
Honzawa-Ryu <benzelongji@gmail.com>
2026-03-30 13:03:30 +0900

    refactor main.sf etc

--- main.sh
+++ main.sh
@@ -10,28 +10,19 @@
 set -euo pipefail
 
 # Scrach領域を利用する
-# 環境変数として.bashrcを宣言
-# source ~/.bashrc
+# 環境変数として.workspace_rcを宣言
+source ../.workspace_rc
 PROJECT_NAME=$(basename "$SLURM_SUBMIT_DIR")
 
-# projectのディレクトリがあるか確認して、なければ作成する
-PROJECT_DIR="/mnt/HDD/$USER/$PROJECT_NAME"
+# projectのディレクトリが計算ノード側にあるか確認して、なければ作成する
+PROJECT_DIR="/scratch/$USER/$PROJECT_NAME"
 if [ ! -d "$PROJECT_DIR" ]; then
     echo "Creating project directory at $PROJECT_DIR"
     mkdir -p "$PROJECT_DIR"
 fi
 
-EXCLUDES=(
-    '--exclude=*.log'
-    '--exclude=*.sif'
-    '--exclude=.uv_cache'
-    '--exclude=.venv'
-    '--exclude=outputs'
-    '--exclude=results'
-    '--exclude=__pycache__'
-)
-# ディレクトリごとrsyncを行う
-rsync -av "${EXCLUDES[@]}" . "/mnt/HDD/$USER/$PROJECT_NAME"
+# 必要なファイルを計算ノード側のプロジェクトディレクトリに転送する
+rsync -av scripts src env.sif pyproject.toml uv.lock data* "$PROJECT_DIR/"
 
 # データが圧縮されている場合は展開する
 if [ -f "$PROJECT_DIR/data.tar.gz" ] && [ ! -d "$PROJECT_DIR/data" ]; then
@@ -59,7 +50,6 @@ exec > >(tee -a "${OUT_DIR}/stdout.log")
 exec 2> >(tee -a "${OUT_DIR}/stderr.log" >&2)
 
 # 実行スクリプト、実行した際のgitコミット、そこからの差分を記録
-
 cp "$0" "$OUT_DIR/job_script.sh"
 git rev-parse HEAD > "$OUT_DIR/git_commit.txt"
 git diff > "$OUT_DIR/git_diff.patch"
@@ -68,9 +58,11 @@ git diff > "$OUT_DIR/git_diff.patch"
 ln -snf "$(realpath $OUT_DIR)" "$PROJECT_DIR/latest"
 
 # コード実行
+# 実験の内容に応じて、00_test.pyの部分を適切なスクリプトに置き換える
 apptainer exec --nv env.sif bash -c "uv run python scripts/00_test.py --out $OUT_DIR"
 
-rsync -av "$OUT_DIR" "/workspace/filesrv01/$USER/$PROJECT_NAME/results/"
+# 結果をCache領域に転送
+rsync -av "$OUT_DIR" "$SLURM_SUBMIT_DIR/results/"
 
 # データセットや出力だけを消し、.venv と .uv_cache は維持
 find "$PROJECT_DIR" -maxdepth 1 ! -name '.venv' ! -name '.uv_cache' ! -name '.' -exec rm -rf {} +
--- scripts/00_test.py
+++ scripts/00_test.py
@@ -1,3 +1,9 @@
+"""
+テスト用スクリプト
+main.shを実行し、このスクリプトを呼び出すとresukts/yymmdd_hhmm_jobid/test.txtを作成してSccess!と書き込む。
+その後results以下がrsyncされて管理ノード側のディレクトリ内に現れることを確認できれば問題なく利用可能です。
+"""
+
 import argparse
 import os
 
@@ -8,8 +14,7 @@ args = parser.parse_args()
 # 書き出し先のパスを作成
 path = os.path.join(args.out, "test.txt")
 
-# ディレクトリが存在することを確認（念のため）
 os.makedirs(args.out, exist_ok=True)
 
-with open(path, mode='w') as f: # 'x'モードでもOK
+with open(path, mode='w') as f: 
     f.write("Success!")
